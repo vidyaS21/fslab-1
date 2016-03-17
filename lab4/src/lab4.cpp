9dae1b243b13e68e2a996d584e79244b9cf21244
vidyaS21 <vidyasadashiv21@gmail.com>
2016-03-17 16:06:32 +0530

    Update lab4.cpp
    done

--- lab4/src/lab4.cpp
+++ lab4/src/lab4.cpp
@@ -6,10 +6,126 @@
 // Description : Hello World in C++, Ansi-style
 //============================================================================
 
-#include <iostream>
+#include<iostream>
+#include<sstream>
+#include<string>
+#include<fstream>
+#include<stdlib.h>
 using namespace std;
-
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+class student
+{
+	string usn,name,branch,sem,buffer;
+public:
+	void read_data();
+	void display_data();
+	void pack();
+	void unpack(int);
+}s;
+int rrn[100],count = 0;
+fstream fp;
+void find_rrn();
+void search();
+void error(int);
+void find_rrn()
+{
+	int pos;
+	string buffer;
+	fp.open("data.txt",ios::in);
+	if(!fp)
+		error(1);
+	while(fp)
+	{
+		pos = fp.tellg();
+		getline(fp,buffer);
+		if(buffer.length()==0)
+			continue;
+		rrn[++count]=pos;
+	}
+	fp.close();
+}
+void student::read_data()
+{
+	cout<<"\nEnter usn: ";
+	cin>>usn;
+	cout<<"\nEnter name: ";
+	cin>>name;
+	cout<<"\nEnter branch";
+	cin>>branch;
+	cout<<"\nEnter sem: ";
+	cin>>sem;
+}
+void student::pack()
+{
+	int pos = fp.tellg();
+	string buffer=usn+'|'+name +'|'+branch +'|'+sem+'|'+'$';
+	fp<<buffer<<endl;
+	rrn[++count]=pos;
+}
+void student::unpack(int pos)
+{
+	fp.seekg(pos, ios::beg);
+	getline(fp, usn, '|');
+	getline(fp, name, '|');
+	getline(fp, branch, '|');
+	getline(fp, sem, '|');
+}
+void student::display_data()
+{
+	cout<<"\nName: "<<name<<"\nusn: "<<usn<<"\nbranch: "<<branch<<"\nsem: "<<sem<<endl;
+}
+void search()
+{
+	int rrn_srch, pos;
+	cout<<"Enter the RRN of the record to be found:\n";
+	cin>>rrn_srch;
+	if(rrn_srch>count||rrn_srch<1)
+	{	error(2);
+	return;
+	}
+	cout<<"Record Found:\n";
+	pos=rrn[rrn_srch];
+	cout<<pos;
+	fp.open("data.txt", ios::in);
+	if(!fp)
+		error(1);
+	s.unpack(pos);
+	fp.close();
+	s.display_data();
+}
+void error(int error)
+{
+	switch(error)
+	{
+	case 1: cout<<"Unable to open the record File\n";
+	exit(0);
+	case 2:	cout<<"Invalid RRN\n";
+	return;
+	}
+}
+int main()
+{
+	int choice;
+	fp.open("data.txt",ios::out|ios::app);
+	fp.close();
+	find_rrn();
+	while(1)
+	{
+		cout<<"1. Insert a record\n2. Search for record using RRN\n3. Exit\nEnter Choice : ";
+		cin>>choice;
+		switch(choice)
+		{
+		case 1:
+			s.read_data();
+			fp.open("data.txt",ios::out|ios::app);
+			if(!fp)
+				error(1);
+			s.pack();
+			fp.close();
+			break;
+		case 2:
+			search();
+			break;
+		default: exit(0);
+		}
+	}
 }
